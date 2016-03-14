c008d71e0b48a49e59470b0a1215d52272f21996
Antoine <antoine.rigoureau@eleves.ec-nantes.fr>
2016-03-14 18:05:14 +0100

    Fait jusqu'à la 2.2.4 qui est à finir

--- Utilities.py
+++ Utilities.py
@@ -2,8 +2,9 @@
 
 """
 Les fonctions presentes sont:
-- copulaCreation
-- distributionCreation
+- copulaCreation qui va creer la copule pour X
+- distributionCreation qui va creer les distributions de X
+- la fonction f, permettant d'obtenir la variable Y a partir du vecteur aleatoire X
 """
 
 from openturns import *
@@ -14,18 +15,45 @@ def copulaCreation():
 	R = CorrelationMatrix(2)
 	R[1,0] = R[0,1] = 2*np.sin((pi/6)*0.17)
 	R[1,1] = R[0,0] = 2*np.sin(pi/6)
-	copulas = ComposedCopula([IndependantCopula(2),NormalCopula(2,R])
+	copulas = ComposedCopula([IndependentCopula(2),NormalCopula(R)])
 	return(copulas)
 	
 
 
 def distributionCreation(situation):
+	D1 = Gamma(3.6239,(1./134.827),0)
+	D2 = TruncatedDistribution(Gumbel(6.8951*np.exp(-4),663.75),0,TruncatedDistribution.LOWER)
+	D3 = RandomMixture([D1,D2],[0.85,0.15])
 	if (situation == 1):
-		Q = Gamma(3.6239,1/134.827,0)
+		Q = D1
 	if (situation == 2):
-		Q = TruncatedDistribution(Gumbel(6.8951*np.exp(-4),663.75),0,side=TruncatedDistribution.LOWER)
-	if (situation == 3) 
-		Q = RandomMixture
-		
+		Q = D2
+	if (situation == 3):
+		Q = D3
+	K = TruncatedDistribution(Normal(30,7.5),0,TruncatedDistribution.LOWER)
+	Zv = Triangular(49,50,51)
+	Zm = Triangular(54,55,56)
+	return([Q,K,Zv,Zm])
+
+
+
+def f(X,L,B):
+	Y = (X.getMarginal(3) - X.getMarginal(2))/L
+	Y.sqrt()
+	Y = ProductDistribution(Y, X.getMarginal(1)*B)
+	inv = NumericalMathFunction('x','1.0/x')
+	b0 = Y.getRange().getLowerBound()
+	bN = Y.getRange().getUpperBound()
+	a = [b0[0],bN[0]]
+	g = [SpecFunc.MaxNumericalScalar, inv(bN)[0]]
+	Y = CompositeDistribution(inv,Y,a,g)
+	Y = ProductDistribution(Y,X.getMarginal(0))
+	a0 = Y.getRange().getLowerBound()
+	aN = Y.getRange().getUpperBound()
+	puiss = NumericalMathFunction('x','x^(3./5.)')
+	a = [a0[0],aN[0]]
+	g = [puiss(a0)[0], puiss(aN)[0]]
+	Y = CompositeDistribution(puiss,Y,a,g)
+	return(Y)
 	
 			
--- main.py
+++ main.py
@@ -1,1 +1,57 @@
-#Fichier principal pour le projet de QI
+#Fichier principal pour le projet
+
+from openturns import *
+import numpy as np
+from math import *
+from Utilities import *
+import matplotlib.pyplot as plt
+from openturns.viewer import View
+
+#Creation de la copule de X
+copula = copulaCreation()
+
+#Creation des distributions de X
+situation = 1
+distributions = distributionCreation(situation)
+
+#Creation de X
+X = ComposedDistribution(distributions,copula)
+
+#Moyennes et Variances des loies marginales
+for i in range(len(distributions)):
+	mean = X.getMarginal(i).getMean()[0]
+	var =  X.getMarginal(i).getMoment(2)[0] - mean*mean
+	print("Moyenne de la marginale " + str(i) + " :"+ str(mean))
+	print("Variance de la marginale " + str(i) + " :"+ str(var))
+
+
+#Graph des densites marginales
+#TODO: faire des jolis graph
+#Pour voir les graph, ajouter .show() a la fin de la derniere linge du bloc
+for i in range (len(distributions)):
+	pdf_graph = X.drawMarginal1DPDF(i,0,100,1000)
+	fig = plt.figure(figsize=(10, 5))
+	pdf_axis = fig.add_subplot(111)
+	View(pdf_graph, figure=fig, axes=[pdf_axis], add_legend=False).show()
+	
+
+#Calcul des matrices de cavariances, de Spearman et tau de Kendall de X
+Xcov = X.getCovariance()
+Xspea = X.getSpearmanCorrelation()
+Xk = X.getKendallTau()
+print("Matrice de covariance:")
+print(Xcov)
+print("Matrice de Spearman:")
+print(Xspea)
+print("Matrice tau de Kendall:")
+print(Xk)
+
+
+#Creation de la valeur d'interet: variable aleatoire Y
+Y = f(X,100,10)
+
+#Evaluations de Y et calcul de moyenne/ecart-type
+Yi = Y.getSample(10000)
+mean = sum(np.array(Yi))/10000
+print(mean)
+
